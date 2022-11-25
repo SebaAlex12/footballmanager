5bb75cff9be6a9c874beea393508e646825e22ce
BlumoMariusz <86651002+BlumoMariusz@users.noreply.github.com>
2022-11-25 11:25:17 +0100

    s

--- client/src/components/matches/MatchItem.js
+++ client/src/components/matches/MatchItem.js
@@ -191,7 +191,7 @@ class MatchItem extends Component {
             <p className="text-white mb-0">
               [{counter}] 
               Termin rozgrywki: {}
-              { <Moment format="YYYY-MM-DD HH:mm">{moment(match.date).subtract(2, 'hours')}</Moment> }
+              { <Moment format="YYYY-MM-DD HH:mm">{moment(match.date).subtract(12, 'hours')}</Moment> }
               {/* <span>{ match.date }</span> */}
               { matchBettingUser.length === 1 && ( 
                 <span className="card-betting-info">
